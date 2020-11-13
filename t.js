9af9286c706596a956231d5b46aa42282e00862b
tux07 <55962267+tux07@users.noreply.github.com>
2020-11-13 11:39:39 -0800

    Create t.js

---
+++ t.js
@@ -0,0 +1,15 @@
+ (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3239.108 Safari/537.36';
+const COOKIE = [{
+
+        "domain": "chaturbate.com",
+        "expirationDate": '2020-10-11T01:09:05.000Z',
+        "hostOnly": false,
+        "httpOnly": false,
+        "name": "agreeterms",
+        "path": "/",
+        "sameSite": "no_restriction",
+        "secure": false,
+        "session": false,
+        "storeId": "0",
+        "value": "1"
+}];
